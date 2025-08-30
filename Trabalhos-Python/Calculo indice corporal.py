00f4b7077bc1dbbc2e6e54e854d3af36f0f9dc5c
Vitor Hugo Aragão Cairo <vitorhugocairo@gmail.com>
2025-08-30 11:22:36 -0300

    Add files via upload

---
+++ Trabalhos-Python/Calculo indice corporal.py
@@ -0,0 +1,11 @@
+nomecli = input('Digite seu nome: ')
+pesocli = float(input('Digite seu peso: '))
+alturacli = float(input('Digite sua altura: '))
+
+IMC = pesocli/(alturacli*alturacli)
+
+print('Nome do cliente:', nomecli)
+print('Peso do cliente: ', pesocli)
+print('Altura do cliente: ', alturacli)
+print('IMC do cliente', IMC)
+
